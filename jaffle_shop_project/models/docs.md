22d3cc121b2918ddb04ad7af91b77b400656cedf
Kenzy Ahmed <kenzy9201@gmail.com>
2026-05-15 10:40:37 +0000

    Built dbt data warehouse project using DuckDB (staging + marts + tests + documentation)

---
+++ jaffle_shop_project/models/docs.md
@@ -0,0 +1,13 @@
+{% docs orders_status %}
+
+Orders can be one of the following statuses:
+
+| status | description |
+|--------|-------------|
+| placed | The order has been placed but has not yet left the warehouse |
+| shipped | The order has been shipped to the customer |
+| completed | The order has been received by the customer |
+| return_pending | The customer wants to return the order |
+| returned | The order has been returned |
+
+{% enddocs %}
---
+++ jaffle_shop_project/models/staging/stg_payments.sql
@@ -0,0 +1,26 @@
+with source as (
+
+    {#-
+    Normally we would select from the table here,
+    but we are using seeds to load our data in this project
+    #}
+
+    select * from {{ ref('raw_payments') }}
+
+),
+
+renamed as (
+
+    select
+        id as payment_id,
+        order_id,
+        payment_method,
+
+        -- amount is stored in cents, convert to dollars
+        amount / 100 as amount
+
+    from source
+
+)
+
+select * from renamed
